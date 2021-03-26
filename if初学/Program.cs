9ce831318dc14fc45222c31b2a59523597ed06a3
XiaoMouz <1114096791@qq.com>
2021-03-26 22:46:13 +0800

    if、if-else、if else-if的学习

--- if初学/Program.cs
+++ if初学/Program.cs
@@ -6,7 +6,7 @@ namespace _07_if初学
     {
         static void Main(string[] args)
         {
-            //if
+            //if：用于单一条件判断
             #region 题目一:Disenable
             //Q：当输入者年龄大于30时反馈输入者你该结婚了
             //console.writeline("请输入你的年龄");
@@ -39,23 +39,75 @@ namespace _07_if初学
             //    Console.WriteLine("你获得了一顿毒打");
             //}
             #endregion
-            #region 题目三:Enable
-            Console.WriteLine("请输入你的用户名");
-            string username = Console.ReadLine();
-            Console.WriteLine("请输入你的密码");
-            string password = Console.ReadLine();
-            bool allow = (username == "admin") && (password == "mypass");
-            if (allow)
-            {
-                Console.WriteLine("登录成功");
-            }
-            if (allow == false)
-            {
-                Console.WriteLine("密码错误，程序退出");
-            }
+            #region 题目三:Disenable
+            //Console.WriteLine("请输入你的用户名");
+            //string username = Console.ReadLine();
+            //Console.WriteLine("请输入你的密码");
+            //string password = Console.ReadLine();
+            //bool allow = (username == "admin") && (password == "mypass");
+            //if (allow)
+            //{
+            //    Console.WriteLine("登录成功");
+            //}
+            //if (allow == false)
+            //{
+            //    Console.WriteLine("密码错误，程序退出");
+            //}
             #endregion
-            //if-else
-
+            //if-else：用于2种情况的判断
+            #region 题目一:Disenable
+            //若用户成绩大于90分（含），否则挨打
+            //Console.WriteLine("请输入你的成绩");
+            //int score = Convert.ToInt32(Console.ReadLine());
+            //if(score>=90)
+            //{
+            //    Console.WriteLine("逃过一劫，加100元");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("你被打的真惨");
+            //}
+            #endregion
+            #region 题目二:Disenable
+            //若成绩大于90则给予A、大于80则给予B、大于70则给予C、大于60则给予D、小于60则给予不合格（所有大于均等于，且不包含上一级）
+            //Console.WriteLine("请输入成绩");
+            //int score = Convert.ToInt32(Console.ReadLine());
+            //if(score>=90)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else
+            //{
+            //    if(score>=80)
+            //    {
+            //        Console.WriteLine("B");
+            //    }
+            //    else
+            //    {
+            //        if(score>=70)
+            //        {
+            //            Console.WriteLine("C");
+            //        }
+            //        else
+            //        {
+            //            if(score>=60)
+            //            {
+            //                Console.WriteLine("D");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("不及格");
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+            //if-else-if：用于多条件的区间性判断
+            #region example
+            //若成绩大于90则给予A、大于80则给予B、大于70则给予C、大于60则给予D、小于60则给予不合格（所有大于均等于，且不包含上一级）
+            
+            #endregion
+            Console.ReadKey();
         }
     }
 }
