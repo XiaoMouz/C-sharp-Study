2be71e17485e4d47d3dc10233cd88fd1e1533092
XiaoMouz <1114096791@qq.com>
2021-04-08 21:15:59 +0800

    finish class

--- Class02-流程控制/Process control/01-异常处理/Program.cs
+++ Class02-流程控制/Process control/01-异常处理/Program.cs
@@ -6,7 +6,24 @@ namespace _01_异常处理
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //通过try-catch来处理异常
+            bool ans = true;
+            int number = 0;
+            Console.WriteLine("输入数字");
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            catch//如果try内代码报异常则执行catch
+            {
+                Console.WriteLine("error,your input not number");
+                ans = false;
+            }
+            if(ans)//判断是否正常录入了number
+            {
+                Console.WriteLine(number * 2);
+            }
+
         }
     }
 }
