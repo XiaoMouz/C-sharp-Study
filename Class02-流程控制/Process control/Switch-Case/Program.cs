d4d01d0acf1d816d87ec8f3fcdd0212fff1219be
XiaoMouz <1114096791@qq.com>
2021-04-17 00:09:31 +0800

    switch-case

---
+++ Class02-流程控制/Process control/Switch-Case/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //员工评级，若评价为A则+500元工资，若B则+200元工资，若C则不变，若D则-200，若E则-500
+            //困了，混一下Github Stat，明天再学，拜拜
+        }
+    }
+}
