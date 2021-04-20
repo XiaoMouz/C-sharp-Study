d8dc0a52fa9080c4c5924d62e5d227e14917bd14
XiaoMouz <1114096791@qq.com>
2021-04-21 03:04:25 +0800

    good night:)

--- Class02-流程控制/Process control/02-Switch Case/Program.cs
+++ Class02-流程控制/Process control/02-Switch Case/Program.cs
@@ -67,7 +67,143 @@ namespace _02_Switch_Case
             if (past)
             {
                 Console.Write("员工工资修改为{0}", salary);
-            } 
+            }
+            #endregion
+
+            Console.WriteLine("按任意键next");
+            Console.ReadLine();
+
+
+            //评分，当分数≥90时=A，≥80时=B，≥70时=C，≥60时=D，＜60=E
+            #region Q1回答
+            Console.WriteLine("请输入分数");
+            try
+            {
+                int score = Convert.ToInt32(Console.ReadLine());
+                switch (score / 10) //将其转为单位数
+                {
+                    case 10:
+                    case 9:
+                        Console.WriteLine("A");
+                        break;
+                    case 8:
+                        Console.WriteLine("B");
+                        break;
+                    case 7:
+                        Console.WriteLine("C");
+                        break;
+                    case 6:
+                        Console.WriteLine("D");
+                        break;
+                    default:
+                        Console.WriteLine("E");
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("错误");
+            }
+            #endregion
+
+            Console.WriteLine("按任意键next");
+            Console.ReadLine();
+
+
+            //输入月份，显示天数（注意闰年）
+            try
+            {
+
+
+                Console.WriteLine("请输入年份");
+                int year = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+
+
+                    Console.WriteLine("请输入月份");
+                    int month = Convert.ToInt32(Console.ReadLine());
+                    if (month >= 1 && month <= 12)
+                    {
+
+
+
+                        #region Q2回答
+                        //    switch (month)
+                        //    {
+                        //        case 1:
+                        //        case 3:
+                        //        case 5:
+                        //        case 7:
+                        //        case 8:
+                        //        case 10:
+                        //        case 12:
+                        //            Console.WriteLine("本月有31天");
+                        //            break;
+                        //        case 4:
+                        //        case 6:
+                        //        case 9:
+                        //        case 11:
+                        //            Console.WriteLine("本月有30天");
+                        //            break;
+                        //        case 2: //判断年份
+                        //            bool year_yes = false;
+                        //            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+                        //            {
+                        //                Console.WriteLine("已判定为闰年，2月份为29天");
+                        //            }
+                        //            else
+                        //            {
+                        //                Console.WriteLine("已判定非闰年，2月份为28天");
+                        //            }
+                        //            break;
+                        //    }
+                        //}
+                        #endregion
+
+                        #region Q2答案
+                        int day = 0;
+                        switch (month)
+                        {
+                            case 1:
+                            case 3:
+                            case 5:
+                            case 7:
+                            case 8:
+                            case 10:
+                            case 12:
+                                day = 31;
+                                break;
+                            case 2:
+                                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                                {
+                                    day = 29;
+                                }
+                                else
+                                {
+                                    day = 28;
+                                }
+                                break;
+                            default:
+                                day = 30;
+                                break;
+                        }
+                        Console.WriteLine("{0}年{1}月有{2}天", year, month, day);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error:month input");
+                    }
+                }
+                catch//月检查
+                {
+                    Console.WriteLine("error:month input");
+                }
+            }
+            catch//年检查
+            {
+                Console.WriteLine("error:year input");
+            }
             #endregion
         }
     }
---
+++ Class02-流程控制/Process control/03-循环结构while/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03_循环结构while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //睡大觉，明天学:https://www.bilibili.com/video/BV1FJ411W7e5?p=41&spm_id_from=pageDriver
+        }
+    }
+}
