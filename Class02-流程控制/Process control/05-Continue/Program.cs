f01ac622b8a57705997943c242059467dd9daf5a
XiaoMouz <1114096791@qq.com>
2021-05-11 02:42:08 +0800

    fixed something

--- Class02-流程控制/Process control/05-Continue/Program.cs
+++ Class02-流程控制/Process control/05-Continue/Program.cs
@@ -7,6 +7,9 @@ namespace _05_continue
         static void Main(string[] args)
         {
             //Continue用于在循环中立即回到判断过程
+            //使用Continue找出100内所有素数
+
+
         }
     }
 }
---
+++ Class02-流程控制/Process control/06-for循环/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06_for循环
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //输入for然后按下两次tab
+            //输入forr然后按下两次tab便是i-- for循环
+            //在控制台打印10次Hello,world
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello,world");
+            //}
+            //for循环一般用于已知循环次数的循环 条件仅能为数字
+            //while循环一般用于未知循环次数的循环 只需bool条件为true
+            //for循环循环条件为固定顺序
+            //int i=0只执行一次
+            //i<10会执行多次来判断
+            //当循环体结束会执行i++
+
+            //求1-100的整数和
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
