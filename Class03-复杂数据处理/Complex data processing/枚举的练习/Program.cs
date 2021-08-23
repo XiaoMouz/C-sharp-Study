379865fc093c7fc315e985d7b9aa949f09678b83
XiaoMouz <1114096791@qq.com>
2021-08-23 20:34:09 +0800

    check youself

--- Class03-复杂数据处理/Complex data processing/枚举的练习/Program.cs
+++ Class03-复杂数据处理/Complex data processing/枚举的练习/Program.cs
@@ -11,16 +11,34 @@ namespace 枚举的练习
     }
     public enum Internet
     {
-        Online,
+        Online=1,
         Offline,
-        Wrong
+        Busy
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Sesons s = Sesons.夏;
-            Internet now = Internet.Online;
+            //Sesons s = Sesons.夏;
+            //Internet now = Internet.Online;
+
+
+            //状态显示
+            Console.WriteLine("请选择您的状态\n1-在线、2-离线、3-忙碌\n回复数字即可");
+            string choose = Console.ReadLine();
+            switch (choose)
+            {
+                case "1": Internet state_1 = (Internet)Enum.Parse(typeof(Internet), choose);
+                    Console.WriteLine("您选择的状态是{0}", state_1);
+                    break;
+                case "2": Internet state_2=(Internet)Enum.Parse(typeof(Internet),choose);
+                    Console.WriteLine("您选择的状态是{0}", state_2);
+                    break;
+                case "3":Internet state_3 = (Internet)Enum.Parse(typeof(Internet), choose);
+                    Console.WriteLine("您选择的状态是{0}", state_3);
+                    break;
+
+            }
         }
     }
 }
---
+++ Class03-复杂数据处理/Complex data processing/结构的练习/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace 结构的练习
+{
+    public struct MyColor
+    {
+        public int _red;
+        public int _blue;
+        public int _green;
+    }
+
+    public enum Gender
+    {
+        男,
+        女
+    }
+
+    public struct Person
+    {
+        public string _name;
+        public int _age;
+        public Gender _gender;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 颜色
+            //定义MyColor结构，设定int类型的红蓝绿
+            //再声明一个MyColor类型的变量，并对成员赋值，使MyColor可以表示成红色
+            //MyColor mc;
+            //mc._red = 255;
+            //mc._blue = 0;
+            //mc._green = 0;
+            #endregion
+
+
+            //定义一个结构类型Person，有三个成员（姓名、年龄、性别），性别使用枚举
+            //声明其变量，分别表示 张三/男/18 李四/女/16
+
+            Person 张三;
+            张三._name = "张三";
+            张三._age = 18;
+            张三._gender = Gender.男;
+
+            Person 李四;
+            李四._name = "李四";
+            李四._age = 16;
+            李四._gender = Gender.女;
+
+            Console.WriteLine("我叫{0},{1}岁，性别{2}", 张三._name, 张三._age, 张三._gender);
+            Console.WriteLine("我叫{0},{1}岁，性别{2}", 李四._name, 李四._age, 李四._gender);
+        }
+    }
+}
