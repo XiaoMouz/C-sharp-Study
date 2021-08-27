efb532bcfc4ecd280a717e0c71f70b4eca2749e7
XiaoMouz <1114096791@qq.com>
2021-08-27 23:39:50 +0800

    睡前提交

--- Class04-方法(函数)/Function/02-方法的调用问题/Program.cs
+++ Class04-方法(函数)/Function/02-方法的调用问题/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
 namespace _02_方法的调用问题
+    //我们在Main()函数中调用Test函数，我们管Main函数称为调用者，Test为被调用者
+    //被调用者想要得到调用者的值
+    //有两个方法：1、传参   2、使用静态字段来模拟全局变量（C#没有全局变量）
 {
     class Program
     {
+        public static int _number = 10;  //静态字段
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int a = 3;
+            //Test();
+            //Console.WriteLine();
+        }
+        public static void Test(int a)
+        {
+            a = a + 5; //无法访问到Main内的a
+        }
+
+
+        public static void TestTwo()
+        {
+
         }
     }
 }
