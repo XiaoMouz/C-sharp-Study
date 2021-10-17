eb3c309ae2891c47dac920a0eeeff4619aefd2a3
XiaoMouz <1114096791@qq.com>
2021-10-17 23:58:01 +0800

    我先睡了

--- Class04-方法(函数)/Function/ref练习/Program.cs
+++ Class04-方法(函数)/Function/ref练习/Program.cs
@@ -6,7 +6,9 @@ namespace ref练习
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Q:使用ref交换两个int类型变量
+            int a = 10;
+            int b = 20;
         }
     }
 }
