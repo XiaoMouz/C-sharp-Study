3806d09d2f748d04ad451e69364f2e42f49a805a
XiaoMouz <1114096791@qq.com>
2021-11-01 22:40:57 +0800

    马上就写飞行棋了，属实摸的一手好鱼

---
+++ Class04-方法(函数)/Function/07-方法的递归/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07_方法的递归
+{
+    class Program
+    {
+        public static int i = 0;
+        static void Main(string[] args)
+        {
+            //方法自己调用自己
+            TellWho();
+            Console.WriteLine(i);
+        }
+
+
+        public static void TellWho()
+        {
+            //倘若在方法内声明变量用于判断则会导致变量被重置为0，因此需要使用局部静态变量(?)
+            //int i = 0;
+            Console.WriteLine("Hello,World");
+            Console.WriteLine(":)");
+            if (i >= 0)
+            {
+                return;
+            }
+            TellWho();
+            //在每次递归时，return并没有执行，在满足i >= 0这个条件后才会执行递归
+        }
+    }
+}
---
+++ Class04-方法(函数)/Function/方法的复习/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 方法的复习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
