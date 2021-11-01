9b518f91f3690523c2fd9ccb9e86a68a87f0bd07
XiaoMouz <1114096791@qq.com>
2021-11-01 22:46:24 +0800

    综合练习，丢笔记本上继续写

--- Class04-方法(函数)/Function/方法的总综合练习/Program.cs
+++ Class04-方法(函数)/Function/方法的总综合练习/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace 方法的复习与练习
+namespace 方法的总综合练习
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
