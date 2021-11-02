b1ad31c65e0d927e8f0f004a3e720919c61d5391
XiaoMouz <1114096791@qq.com>
2021-11-02 18:48:24 +0800

    写个头儿

--- Class05-飞行棋/Flight chess/01-游戏头/Program.cs
+++ Class05-飞行棋/Flight chess/01-游戏头/Program.cs
@@ -6,7 +6,32 @@ namespace _01_游戏头
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameMenu();
         }
+
+
+        /// <summary>
+        /// 游戏LOGO
+        /// </summary>
+        public static void GameMenu()
+        {
+            //Console.BackgroundColor = ConsoleColor.Gray; <这玩意太丑了，不想用
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("----------------------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("----------------------");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("----------------------");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("--Flight Chess  v1.0--");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("----------------------");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("----------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("----------------------");
+        }
+        
+
     }
 }
