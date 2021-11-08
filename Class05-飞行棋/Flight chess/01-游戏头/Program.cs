2ce641586fd17b02e72360be42dc72eb94f90fa6
XiaoMouz <1114096791@qq.com>
2021-11-08 22:01:33 +0800

    额...初始化

--- Class05-飞行棋/Flight chess/01-游戏头/Program.cs
+++ Class05-飞行棋/Flight chess/01-游戏头/Program.cs
@@ -10,6 +10,8 @@ namespace _01_游戏头
         static void Main(string[] args)
         {
             GameMenu();
+            InitializationMap();
+
         }
 
 
@@ -35,9 +37,56 @@ namespace _01_游戏头
             Console.WriteLine("----------------------");
         }
         
-        public static void M1()
+        //其它解法，保留
+        //public static void MapPrintf()
+        //{
+        //    int[] market = { 4, 6, 11, 56 };
+        //    for (int i = 0; i < Maps.Length; i++)
+        //    {
+        //            if (Maps[i]==market[i])
+        //            {
+        //                Console.ForegroundColor = ConsoleColor.Red;
+        //                Console.Write("A");
+        //            }
+        //            else
+        //            {
+        //                Console.ForegroundColor = ConsoleColor.Gray;
+        //                Console.Write("-");
+        //            }
+        //    }
+        //}
+
+        /// <summary>
+        /// 地图初始化，将地图打上标记
+        /// </summary>
+        public static void InitializationMap()
         {
+            int[] luckyturn = { 6, 17, 48, 53, 88 };
+            for (int i = 0; i < luckyturn.Length; i++)
+            {
+                Maps[luckyturn[i]] = 1;
+            }
+
+            int[] landMine = { 4, 10, 31, 66, 72, 91 };
+            for (int i = 0; i < landMine.Length; i++)
+            {
+                Maps[landMine[i]] = 1;
+            }
+
+            int[] pause = { 9, 23, 57, 60, 80 };
+            for (int i = 0; i < pause.Length; i++)
+            {
+                Maps[pause[i]] = 1;
+            }
+
+            int[] tunnel = { 14, 42, 65, 85, 90 };
+            for (int i = 0; i < tunnel.Length; i++)
+            {
+                Maps[tunnel[i]] = 1;
+            }
 
         }
+
+
     }
 }
