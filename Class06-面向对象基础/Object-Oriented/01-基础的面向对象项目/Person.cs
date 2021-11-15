aea928cc86b1fd81d9d7b2a56f3e7b296ee4f0bb
XiaoMouz <1114096791@qq.com>
2021-11-15 22:58:55 +0800

    诶嘿，commits，多亏了之前没提交

--- Class06-面向对象基础/Object-Oriented/01-基础的面向对象项目/Person.cs
+++ Class06-面向对象基础/Object-Oriented/01-基础的面向对象项目/Person.cs
@@ -11,12 +11,24 @@ namespace _01_基础的面向对象项目
     public class Person
     {
         public string _name;//姓名
+        public string Name//透过属性保护字段
+        {
+            get { return _name; }//控制取值
+            set { _name = value; }//控制赋值
+        }
+
         public int _age;//年龄
         public char _gender;//性别
 
-        public void CHLSS()
+        public void CHLSS()//非静态方法
         {
             Console.WriteLine("{0}是一位{1}岁的{2}性，能够执行CHLSS这些活动",this._name,this._age,this._gender);//this表示这个类的对象
         }
+
+        public static void CHALL()//静态方法
+        {
+
+        }
+
     }
 }
--- Class06-面向对象基础/Object-Oriented/01-基础的面向对象项目/Program.cs
+++ Class06-面向对象基础/Object-Oriented/01-基础的面向对象项目/Program.cs
@@ -8,13 +8,16 @@ namespace _01_基础的面向对象项目
     //  属性;
     //  方法;
     //}
+    
     //类不占内存，对象占用内存
     internal class Program
     {
         static void Main(string[] args)
         {
+            string s;//string事系统提供类
+
             //创建Person类的对象
-            Person aNewPerson = new Person();
+            Person aNewPerson = new Person();//自定义类
             aNewPerson._name = "小A";
             aNewPerson._age = 13;
             aNewPerson._gender = '男';
