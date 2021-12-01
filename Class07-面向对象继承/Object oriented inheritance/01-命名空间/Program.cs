a7f2560ecf33a7ce12ca8be3160482321a551df1
XiaoMouz <1114096791@qq.com>
2021-12-01 20:24:34 +0800

    namespace

---
+++ Class07-面向对象继承/Object oriented inheritance/01-命名空间/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_命名空间
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
