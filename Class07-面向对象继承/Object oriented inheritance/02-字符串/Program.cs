8ba76bcdd6c4a2cdf458847c6896c2e93d84d814
XiaoMouz <1114096791@qq.com>
2021-12-04 01:34:34 +0800

    堆堆堆，栈栈栈

---
+++ Class07-面向对象继承/Object oriented inheritance/02-字符串/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_字符串
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1、字符串的不可变性在Note.md中
+        }
+    }
+}
