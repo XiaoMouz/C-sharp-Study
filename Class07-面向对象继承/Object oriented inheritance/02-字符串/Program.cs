aa3064b266626bd2993c16e16b9a9a859e434e8e
XiaoMouz <1114096791@qq.com>
2021-12-04 01:55:33 +0800

    shit

--- Class07-面向对象继承/Object oriented inheritance/02-字符串/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/02-字符串/Program.cs
@@ -7,6 +7,13 @@ namespace _02_字符串
         static void Main(string[] args)
         {
             //1、字符串的不可变性在Note.md中
+            //2、string可以看做char类型的只读数组
+            string pp2 = "abcdefg";
+            //通过下标访问字符串某一元素
+            Console.WriteLine(pp2[0]);
+            //运行即可
+
+
         }
     }
 }
