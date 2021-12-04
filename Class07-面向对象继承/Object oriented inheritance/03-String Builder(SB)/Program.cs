c46ef3a8cef327b6870a62a184e436ca656a6c75
XiaoMouz <1114096791@qq.com>
2021-12-04 16:37:03 +0800

    SBhahhah

--- Class07-面向对象继承/Object oriented inheritance/03-String Builder(SB)/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/03-String Builder(SB)/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Diagnostics;
 
 namespace _03_String_Builder_SB_
 {
@@ -6,7 +8,20 @@ namespace _03_String_Builder_SB_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //String Builder是一个类，可以通过以下创建对象
+            StringBuilder sb = new StringBuilder();
+
+            string s = null;
+            Stopwatch runningTime = new Stopwatch();//创建计时器，需要System.Diagnostics 
+            runningTime.Start();//计时开始
+            for (int i = 0; i < 10000; i++)
+            {
+
+                s += i;//连接、重新赋值
+            }
+            runningTime.Stop();//计时结束
+            Console.WriteLine(runningTime.Elapsed);
+            Console.WriteLine(s);
         }
     }
 }
