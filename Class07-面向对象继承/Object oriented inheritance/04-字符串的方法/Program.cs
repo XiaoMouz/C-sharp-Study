3449ee7a89544eb299d970c37c01ea16d2f9d174
XiaoMouz <1114096791@qq.com>
2021-12-04 19:04:10 +0800

    bjik huihjiojiok

--- Class07-面向对象继承/Object oriented inheritance/04-字符串的方法/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/04-字符串的方法/Program.cs
@@ -6,7 +6,11 @@ namespace _04_字符串的方法
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //nums.length
+            string value = "djiwai";
+            Console.WriteLine("{0}",value.Length);
+
+
         }
     }
 }
