a91dce07b5a9290f569c7563927744c0b0978f7c
XiaoMouz <1114096791@qq.com>
2021-12-04 16:03:35 +0800

    有点憨批

--- Class07-面向对象继承/Object oriented inheritance/02-字符串/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/02-字符串/Program.cs
@@ -12,8 +12,15 @@ namespace _02_字符串
             //通过下标访问字符串某一元素
             Console.WriteLine(pp2[0]);
             //运行即可
-
-
+            //pp2[0]='b'; 并不能给字符串下标为0的字符重新编辑 （此处体现了字符串的不可变性）
+            //若要编辑可以通过以下方式来进行
+            char[] ch = pp2.ToCharArray();//将string转为字符数组
+            ch[0] = 'b';
+            //而后通过创建对象方法来赋值
+            pp2 = new string(ch);
+            Console.WriteLine(pp2[0]);
+            Console.WriteLine(pp2);
+            //以上
         }
     }
 }
