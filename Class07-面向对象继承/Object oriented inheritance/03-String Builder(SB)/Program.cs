dcf761e76b05266ce1c0835cfe0d08e546ce3769
XiaoMouz <1114096791@qq.com>
2021-12-04 16:09:22 +0800

    SB，哈哈哈哈

---
+++ Class07-面向对象继承/Object oriented inheritance/03-String Builder(SB)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_String_Builder_SB_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
