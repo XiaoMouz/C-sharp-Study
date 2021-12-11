380809cba885f17145551b6a50a7913148386449
XiaoMouz <1114096791@qq.com>
2021-12-11 20:31:43 +0800

    水个commit

--- Class07-面向对象继承/Object oriented inheritance/04-字符串的方法/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/04-字符串的方法/Program.cs
@@ -7,17 +7,19 @@ namespace _04_字符串的方法
     {
         static void Main(string[] args)
         {
-            //nums.Length
+            //nums.Length 长度
             string value = "djiwai";
             Console.WriteLine("{0}",value.Length);
-            //nums.ToUpper
+
+            //nums.ToUpper 转大写
             value = value.ToUpper();
             Console.WriteLine(value);
-            //nums.ToLower
+            //nums.ToLower 转小写
             value = value.ToLower();
             Console.WriteLine(value);
-            //nums.Equals
-            if (value.Equals("Djiwai",StringComparison.OrdinalIgnoreCase))
+
+            //nums.Equals 对比
+            if ( value.Equals("Djiwai",StringComparison.OrdinalIgnoreCase) ) //StringComparison.OrdinalIgnoreCase 是选择需要进行对比的类型
             {
                 Console.WriteLine("1");
             }
@@ -25,11 +27,11 @@ namespace _04_字符串的方法
             {
                 Console.WriteLine("0");
             }
-            //nums.Split
+            //nums.Split 分割
             value = "dkowkdo++==  @@ ADPAP";
             char[] redgo = { '+', '=', ' ', '@' };
             //string[] s = value.Split(redgo);Console.WriteLine(s);//这是将redgo中的字符全部分割掉，但会保留空字符
-            string[] s = value.Split(redgo,StringSplitOptions.RemoveEmptyEntries);Console.WriteLine(s); //分割，但是通过RemoveEmptyEntries来清空空字符
+            string[] s = value.Split(redgo,StringSplitOptions.RemoveEmptyEntries);Console.WriteLine(s); //与上文注释一致的分割，然后通过RemoveEmptyEntries来清空空字符
 
 
             //练习：用户输入2008-01-16，将其显示为2008年1月16日
@@ -37,6 +39,8 @@ namespace _04_字符串的方法
             char[] del = { '-' };
             string[] sdata = date.Split(del, StringSplitOptions.RemoveEmptyEntries);
             Console.WriteLine("{0}年{1}月{2}日",sdata[0],sdata[1],sdata[2]);
+
+            //nums.replace 替换
         }
     }
 }
