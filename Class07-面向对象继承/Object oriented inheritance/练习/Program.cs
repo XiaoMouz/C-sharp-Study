f13905b5be94b1897770d90e149712e9bca7d07f
XiaoMouz <1114096791@qq.com>
2021-12-15 20:07:18 +0800

    ..

---
+++ Class07-面向对象继承/Object oriented inheritance/练习/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 练习
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
