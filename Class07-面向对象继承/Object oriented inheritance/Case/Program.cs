18a6bf5cfcf97b1b9e272b1ddf88d493605d40ff
XiaoMouz <1114096791@qq.com>
2021-12-26 19:39:15 +0800

    有病吧

--- Class07-面向对象继承/Object oriented inheritance/Case/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/Case/Program.cs
@@ -6,7 +6,7 @@ namespace Case
     {
         static void Main(string[] args)
         {
-            Ticket _Ticket = new _Ticket.Ticket(114);
+            //Ticket _Ticket = new Ticket.Ticket(114);
             Console.WriteLine("Hello World!");
         }
     }
