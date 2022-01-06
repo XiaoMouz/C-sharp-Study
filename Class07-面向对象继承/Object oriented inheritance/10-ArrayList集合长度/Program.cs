7bbf1acbe01e9a9a85058918eb9697b03447b98d
XiaoMouz <1114096791@qq.com>
2022-01-06 19:36:42 +0800

    1

--- Class07-面向对象继承/Object oriented inheritance/10-ArrayList集合长度/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/10-ArrayList集合长度/Program.cs
@@ -8,9 +8,10 @@ namespace _10_ArrayList集合长度
         static void Main(string[] args)
         {
             ArrayList list = new ArrayList();
-            
+
             //count:表示集合中实际包含的元素个数
-            //capcity:
+            //capacity:表示集合中可以包含的元素个数
+            
         }
     }
 }
