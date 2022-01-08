3a0f7e8ae7847f3e27fe8df487a37d63d734275f
XiaoMouz <1114096791@qq.com>
2022-01-08 22:20:16 +0800

    Create New namespace

---
+++ Class07-面向对象继承/Object oriented inheritance/ArrayList练习/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayList练习
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
