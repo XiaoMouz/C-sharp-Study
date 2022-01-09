04179a86b55b702076723b6aacc6e8d4583ee1e0
XiaoMouz <1114096791@qq.com>
2022-01-09 20:57:49 +0800

    new

--- Class07-面向对象继承/Object oriented inheritance/11-HashTable/Program.cs
+++ Class07-面向对象继承/Object oriented inheritance/11-HashTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace _11_HashTable
 {
@@ -6,7 +7,7 @@ namespace _11_HashTable
     {
         static void Main(string[] args)
         {
-            
+            Hashtable a = new Hashtable();
         }
     }
 }
