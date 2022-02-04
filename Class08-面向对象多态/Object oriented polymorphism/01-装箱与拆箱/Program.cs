2553595c6e1a271b7d8a7707701ee4ba24e61e61
XiaoMouz <1114096791@qq.com>
2022-02-04 17:22:29 +0800

    装箱与拆箱

--- Class08-面向对象多态/Object oriented polymorphism/01-装箱与拆箱/Program.cs
+++ Class08-面向对象多态/Object oriented polymorphism/01-装箱与拆箱/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace _01_装箱与拆箱
 {
@@ -6,7 +7,20 @@ namespace _01_装箱与拆箱
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //装箱
+            int x1 = 1;
+            object y1 = x1;
+
+            //拆箱
+            int x2 = (int)y1;
+        
+            //举例
+            ArrayList list = new ArrayList();
+            //这个循环发生了114514次装箱操作
+            for (int i = 0; i < 114514; i++)
+            {
+                list.Add(i);
+            }
         }
     }
 }
