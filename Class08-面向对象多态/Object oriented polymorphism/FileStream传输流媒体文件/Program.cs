01c832b32c68afa4684921b97e27e14990adf376
XiaoMouz <1114096791@qq.com>
2022-02-06 21:18:31 +0800

    挖坑

---
+++ Class08-面向对象多态/Object oriented polymorphism/FileStream传输流媒体文件/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileStream传输流媒体文件
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
