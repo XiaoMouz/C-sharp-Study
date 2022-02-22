8f4ad23403f161dadd0222ca97262cc56662a07a
XiaoMouz <1114096791@qq.com>
2022-02-22 17:02:09 +0800

    写个小题目

--- Class08-面向对象多态/Object oriented polymorphism/FileStream传输流媒体文件/Program.cs
+++ Class08-面向对象多态/Object oriented polymorphism/FileStream传输流媒体文件/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace FileStream传输流媒体文件
 {
@@ -6,7 +8,45 @@ namespace FileStream传输流媒体文件
     {
         static void Main(string[] args)
         {
-            
+            string source = @"D:\64\My Soul, Your Beats!.mp3";
+            string target = @"D:\64\Target\test.mp3";
+            string successMsg = "Success";
+            string failMsg = "Failed";
+            Console.WriteLine("{0}",CopyMedia(source,target)==0?successMsg:failMsg);
+        }
+
+        public static int CopyMedia(string source, string target)
+        {
+            //创建读取流
+            using(FileStream fileRead = new FileStream(source, FileMode.Open, FileAccess.Read))
+            {
+                //创建写入流
+                using (FileStream fileWrite = new FileStream(target, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    try
+                    {
+                        //缓存字节组
+                        byte[] buffer = new byte[1024 * 1024 * 1];
+
+                        //读取&写入
+                        while (true)
+                        {
+                            int getLength = fileRead.Read(buffer, 0, buffer.Length);
+                            if (getLength == 0)
+                            {
+                                return 0;
+                                
+                            }
+                            fileWrite.Write(buffer, 0, getLength);
+                        }
+                    }
+                    catch
+                    {
+                        return 1;
+                    }
+                    
+                }
+            }
         }
     }
 }
