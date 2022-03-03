12ccaef87b9acf7d4293c9a87285905ee14ffc22
XiaoMouz <1114096791@qq.com>
2022-03-03 21:41:52 +0800

    一点添加

--- Class08-面向对象多态/Object oriented polymorphism/超市收银系统/Warehouse.cs
+++ Class08-面向对象多态/Object oriented polymorphism/超市收银系统/Warehouse.cs
@@ -11,10 +11,23 @@ namespace 超市收银系统
         //存储货物
         //使用ArrayList来存货物
         List<List<ProductFather>> list = new List<List<ProductFather>>();
-        List<ProductFather> productList = new List<ProductFather>();
+        //List<ProductFather> productList = new List<ProductFather>();
         //List<Phone> listPhone = new List<Phone>();
         //List<Milk> listMike = new List<Milk>();
         //List<Laptop> listLaptop = new List<Laptop>();
         //List<Banana> listBanana = new List<Banana>();
+
+        public Warehouse()
+        {
+            list.Add(new List<ProductFather>());//phone
+            list.Add(new List<ProductFather>());//laptop
+            list.Add(new List<ProductFather>());//milk
+            list.Add(new List<ProductFather>());//banana
+        }
+
+        public static void AddProduct(ProductFather in,)
+        {
+
+        }
     }
 }
