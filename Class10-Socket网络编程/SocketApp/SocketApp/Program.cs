3ced61051c1c697e6032ff40719869e4f5fc31d5
XiaoMouz <1114096791@qq.com>
2022-03-24 14:49:53 +0800

    Socket

---
+++ Class10-Socket网络编程/SocketApp/SocketApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket connectWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Any;
+            
+        }
+    }
+}
