dc12d41495ac5eee7e9c2da0744c7d106b677e86
XiaoMouz <1114096791@qq.com>
2022-03-28 22:59:38 +0800

    开始写客户端

---
+++ Class10-Socket网络编程/SocketApp/SocketClient/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SocketServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipInput = new System.Windows.Forms.TextBox();
+            this.portInput = new System.Windows.Forms.TextBox();
+            this.startLinkBtn = new System.Windows.Forms.Button();
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.msgText = new System.Windows.Forms.TextBox();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.chooseFileBtn = new System.Windows.Forms.Button();
+            this.sendFileBtn = new System.Windows.Forms.Button();
+            this.sendMsgBtn = new System.Windows.Forms.Button();
+            this.shockBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ipInput
+            // 
+            this.ipInput.Location = new System.Drawing.Point(62, 65);
+            this.ipInput.Name = "ipInput";
+            this.ipInput.Size = new System.Drawing.Size(100, 21);
+            this.ipInput.TabIndex = 0;
+            // 
+            // portInput
+            // 
+            this.portInput.Location = new System.Drawing.Point(168, 65);
+            this.portInput.Name = "portInput";
+            this.portInput.Size = new System.Drawing.Size(54, 21);
+            this.portInput.TabIndex = 1;
+            // 
+            // startLinkBtn
+            // 
+            this.startLinkBtn.Location = new System.Drawing.Point(246, 63);
+            this.startLinkBtn.Name = "startLinkBtn";
+            this.startLinkBtn.Size = new System.Drawing.Size(75, 23);
+            this.startLinkBtn.TabIndex = 2;
+            this.startLinkBtn.Text = "连接";
+            this.startLinkBtn.UseVisualStyleBackColor = true;
+            this.startLinkBtn.Click += new System.EventHandler(this.startLinkBtn_Click);
+            // 
+            // textLog
+            // 
+            this.textLog.Location = new System.Drawing.Point(62, 107);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.Size = new System.Drawing.Size(386, 96);
+            this.textLog.TabIndex = 4;
+            // 
+            // msgText
+            // 
+            this.msgText.Location = new System.Drawing.Point(62, 218);
+            this.msgText.Multiline = true;
+            this.msgText.Name = "msgText";
+            this.msgText.Size = new System.Drawing.Size(386, 96);
+            this.msgText.TabIndex = 5;
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(62, 341);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(202, 21);
+            this.textInput.TabIndex = 6;
+            // 
+            // chooseFileBtn
+            // 
+            this.chooseFileBtn.Location = new System.Drawing.Point(292, 379);
+            this.chooseFileBtn.Name = "chooseFileBtn";
+            this.chooseFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.chooseFileBtn.TabIndex = 7;
+            this.chooseFileBtn.Text = "选择";
+            this.chooseFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // sendFileBtn
+            // 
+            this.sendFileBtn.Location = new System.Drawing.Point(373, 339);
+            this.sendFileBtn.Name = "sendFileBtn";
+            this.sendFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendFileBtn.TabIndex = 8;
+            this.sendFileBtn.Text = "发送文件";
+            this.sendFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // sendMsgBtn
+            // 
+            this.sendMsgBtn.Location = new System.Drawing.Point(292, 339);
+            this.sendMsgBtn.Name = "sendMsgBtn";
+            this.sendMsgBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendMsgBtn.TabIndex = 9;
+            this.sendMsgBtn.Text = "发送消息";
+            this.sendMsgBtn.UseVisualStyleBackColor = true;
+            // 
+            // shockBtn
+            // 
+            this.shockBtn.Location = new System.Drawing.Point(374, 379);
+            this.shockBtn.Name = "shockBtn";
+            this.shockBtn.Size = new System.Drawing.Size(75, 23);
+            this.shockBtn.TabIndex = 10;
+            this.shockBtn.Text = "震动";
+            this.shockBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shockBtn);
+            this.Controls.Add(this.sendMsgBtn);
+            this.Controls.Add(this.sendFileBtn);
+            this.Controls.Add(this.chooseFileBtn);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.msgText);
+            this.Controls.Add(this.textLog);
+            this.Controls.Add(this.startLinkBtn);
+            this.Controls.Add(this.portInput);
+            this.Controls.Add(this.ipInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ipInput;
+        private System.Windows.Forms.TextBox portInput;
+        private System.Windows.Forms.Button startLinkBtn;
+        private System.Windows.Forms.TextBox textLog;
+        private System.Windows.Forms.TextBox msgText;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button sendFileBtn;
+        private System.Windows.Forms.Button sendMsgBtn;
+        private System.Windows.Forms.Button shockBtn;
+        private System.Windows.Forms.Button chooseFileBtn;
+    }
+}
+
---
+++ Class10-Socket网络编程/SocketApp/SocketClient/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SocketServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startLinkBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Socket socketSend = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(ipInput.Text);
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(portInput.Text));
+                //获得需要连接的远程应用服务器IP地址与端口号
+                socketSend.Connect(point);
+                ShowMsg("连接成功");
+            }
+            catch
+            {}
+        }
+
+        void ListenConnect(object o)
+        {
+            Socket socketWatch = o as Socket;
+            //等待客户端连接 创建一个负责通信的Socket
+            while (true)
+            {
+                try
+                {
+                    Socket socketConnect = socketWatch.Accept();
+                    ShowMsg(socketConnect.RemoteEndPoint.ToString() + ":连接成功");
+
+                    //创建线程用于接收客户端消息
+                    Thread th = new Thread(Recive);
+                    th.IsBackground = true;
+                    th.Start(socketConnect);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        void Recive(object o)
+        {
+
+            Socket socketConnect = o as Socket;
+            //接收客户端消息
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 2];
+                    int getByte = socketConnect.Receive(buffer);//将接收数据存入buffer，getByte是实际收到的字节数
+                    string bufferCoding = Encoding.UTF8.GetString(buffer, 0, getByte);//收到的字符串
+                    if (getByte == 0)
+                    {
+                        break;
+                    }
+                    ShowMsg(socketConnect.RemoteEndPoint + ":" + bufferCoding);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        void ShowMsg(string str)
+        {
+            textLog.AppendText(str+ "\r\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
